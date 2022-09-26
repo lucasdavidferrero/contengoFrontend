3461ea10aad00f57e3390a5d7957d1ac35c53dd5
Lucas Ferrero <lucasdavidferrero@gmail.com>
2022-09-26 19:28:13 -0300

    componente para confirmar alguna acción.

---
+++ src/components/ConfirmDialog.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <q-dialog v-model="confirm" persistent>
+      <q-card>
+        <q-card-section class="row items-center">
+          <q-avatar icon="signal_wifi_off" color="primary" text-color="white" />
+          <span class="q-ml-sm">¿Estas seguro?</span>
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="Cancelar" color="primary" v-close-popup @click="onCancelled"/>
+          <q-btn flat label="Confirmar" color="primary" v-close-popup @click="onConfirm"/>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+export default defineComponent({
+  emits: ['confirmed', 'cancelled'],
+  name: 'ConfirmDialog',
+  setup (_, { emit }) {
+    const confirm = ref(false)
+
+    const onConfirm = () => emit('confirmed')
+    const onCancelled = () => emit('cancelled')
+
+    return {
+      onConfirm,
+      onCancelled,
+      confirm
+    }
+  }
+})
+</script>
--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -6,7 +6,7 @@
       </div>
 
       <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
+        Oops. Nada aquí...
       </div>
 
       <q-btn
