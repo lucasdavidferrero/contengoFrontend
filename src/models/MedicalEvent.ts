749e6633e88b5ae441f4bf33d122276cc6bcdcf4
Lucas Ferrero <lucasdavidferrero@gmail.com>
2022-11-04 08:38:57 -0300

    agrego estructuras de requests y responses.

--- src/models/MedicalEvent.ts
+++ src/models/MedicalEvent.ts
@@ -12,4 +12,25 @@ export namespace MedicalEvent {
     observations: string
     rows: rowDetail[]
   }
+
+  export interface headerCommon {
+    idMascota: string
+    vetName: string
+    observations: string
+  }
+
+  export interface header extends headerCommon {
+    id: number
+  }
+
+  export interface detail {
+    description: string
+    quantity: number
+    unitPrice: number
+  }
+
+  export interface updateReq extends headerCommon {
+    idHeader: number
+    rows: detail[]
+  }
 }
