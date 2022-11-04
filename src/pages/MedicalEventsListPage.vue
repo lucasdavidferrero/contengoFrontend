828daf881d8607daf4487ac08d01712e5ccae18a
Lucas Ferrero <lucasdavidferrero@gmail.com>
2022-11-04 09:01:07 -0300

    Estructura general.

--- src/pages/MedicalEventsListPage.vue
+++ src/pages/MedicalEventsListPage.vue
@@ -1,25 +1,30 @@
 <template>
   <q-page class="q-pa-xl">
     <div>
-      <q-markup-table>
+      <q-markup-table flat bordered>
       <thead>
         <tr>
-          <th class="text-left">Dessert (100g serving)</th>
-          <th class="text-right">Calories</th>
-          <th class="text-right">Fat (g)</th>
-          <th class="text-right">Carbs (g)</th>
-          <th class="text-right">Protein (g)</th>
-          <th class="text-right">Sodium (mg)</th>
+          <th colspan="5">
+            <div class="row no-wrap items-center">
+              <h2 class="text-h6 q-ma-none">Listado de Eventos Médicos</h2>
+            </div>
+          </th>
+        </tr>
+        <tr>
+          <th class="text-left">Ver Detalle</th>
+          <th class="text-right">ID Mascota</th>
+          <th class="text-right">Veterinario</th>
+          <th class="text-right">Observaciones</th>
+          <th class="text-right">Acciones</th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td class="text-left">Frozen Yogurt</td>
-          <td class="text-right">159</td>
-          <td class="text-right">6</td>
-          <td class="text-right">24</td>
-          <td class="text-right">4</td>
-          <td class="text-right">87</td>
+          <td class="text-left"></td>
+          <td class="text-right">5654</td>
+          <td class="text-right">Lucas Ferrero</td>
+          <td class="text-right">bien ahi</td>
+          <td class="text-right">Botones</td>
         </tr>
       </tbody>
     </q-markup-table>
